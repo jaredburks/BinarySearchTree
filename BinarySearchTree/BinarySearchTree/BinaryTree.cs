7d3094a64c24411d385a9d43ca78537257a4157b
jaredburks <jarabu93@gmail.com>
2017-09-27 10:26:55 -0500

    Started methods that will be needed for binarytree class. Completed start of tree method.

--- BinarySearchTree/BinarySearchTree/BinaryTree.cs
+++ BinarySearchTree/BinarySearchTree/BinaryTree.cs
@@ -8,5 +8,36 @@ namespace BinarySearchTree
 {
     class BinaryTree
     {
+        private Node rootNode;
+
+        public BinaryTree()
+        {
+            rootNode = null;
+        }
+
+        public void DisplayNode()
+        {
+
+        }
+
+        public void Add()
+        {
+
+        }
+
+        public void Search()
+        {
+
+        }
+
+        public void StartTree()
+        {
+            rootNode = new Node(10);
+            rootNode.leftChild = new Node(5);
+            rootNode.rightChild = new Node(15);
+            rootNode.leftChild.leftChild = new Node(2);
+            rootNode.leftChild.rightChild = new Node(8);
+            rootNode.rightChild.leftChild = new Node(14);
+        }
     }
 }
