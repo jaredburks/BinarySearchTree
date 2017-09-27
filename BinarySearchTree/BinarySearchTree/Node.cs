0c58d69c76434bd3b6bcc7c8b0c2dcbe0cb94b46
jaredburks <jarabu93@gmail.com>
2017-09-27 09:59:58 -0500

    Finished Node class.

--- BinarySearchTree/BinarySearchTree/Node.cs
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -8,5 +8,14 @@ namespace BinarySearchTree
 {
     class Node
     {
+        public Node leftChild;
+        public Node rightChild;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            leftChild = null;
+            rightChild = null;
+        }
     }
 }
